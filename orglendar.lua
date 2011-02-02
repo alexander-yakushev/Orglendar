c0d81b92005c94ebd855132d578223d8363bb5f3
Alexander Yakushev <yakushev.alex@gmail.com>
2011-02-02 21:03:20 +0200

    Fixed bug with wrong argument name

--- orglendar.lua
+++ orglendar.lua
@@ -134,12 +134,12 @@ local function add_calendar(inc_offset)
 end
 
 function orglendar.register(widget)
-   timewidget:add_signal("mouse::enter", function()
+   widget:add_signal("mouse::enter", function()
                                             add_calendar(0)
                                          end)
-   timewidget:add_signal("mouse::leave", remove_calendar)
+   widget:add_signal("mouse::leave", remove_calendar)
    
-   timewidget:buttons(awful.util.table.join( awful.button({ }, 4, function()
+   widget:buttons(awful.util.table.join( awful.button({ }, 4, function()
                                                                      add_calendar(-1)
                                                                   end),
                                              awful.button({ }, 5, function()
